965cc3eb2fa95d9d2a3f9c0268f891cb32b1c454
Carmine Talpa <cy4gate_ctalpa@elt.it>
2024-12-02 09:55:34 +0100

    fix output variable

--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
 output "vm_user_key" {
-  value = random_password.vm_user_key.result
+  value = tls_private_key.vm_user_key.public_key_openssh
 }
 
 output "vm_user_password" {
   value = random_password.vm_user_password.result
+  sensitive = true
 }
